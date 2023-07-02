4a493a0c3ff82fb07a3b5097728b3102987f047f
Nabell87 <nabilsabir786@gmail.com>
2023-07-02 16:17:11 +0500

    Ui creation
    Browse by property type

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
+import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
 
 function Home() {
@@ -10,6 +11,9 @@ function Home() {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="hometitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="hometitle">Homes guests love</h1>
       </div>
     </div>
   )
